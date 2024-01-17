0f3336c9a601caf371c64960c40c2002da9b5b9d
Demilade111 <demiladealuko111@gmail.com>
2024-01-17 16:25:15 +0100

    Update database configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,21 @@
 import "reflect-metadata";
 import { createConnection, ConnectionOptions } from "typeorm";
-import { Users } from "../entity/User";
 
 const dbConfig: ConnectionOptions = {
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: "dez",
-  password: "123456789",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "recipedb",
   synchronize: true, // Disable synchronize in production
   logging: process.env.NODE_ENV === "development", // Enable logging in development
-  entities: [Users],
+  entities: [
+    process.env.NODE_ENV === "development"
+      ? "src/entity/**/*.ts"
+      : "dist/entity/**/*.js",
+  ],
+
   migrations: [], // Define migrations if needed
   subscribers: [], // Define subscribers if needed
 };
