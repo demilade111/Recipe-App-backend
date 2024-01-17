983c028153ee0c2e992d4ca3fa564ab8d24f5444
Demilade111 <demiladealuko111@gmail.com>
2024-01-17 16:25:41 +0100

    Refactor authentication middleware to handle missing token and invalid token errors

--- src/middlewear/authMiddlewear.ts
+++ src/middlewear/authMiddlewear.ts
@@ -2,37 +2,33 @@
 
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { CustomError } from "../utils/CustomError";
 
 // Assuming the JWT payload is of this type, adjust as needed
 interface JWTPayload {
   userId: number;
   email: string;
-  // add other properties as per your payload structure
 }
 
-const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1];
+interface RequestWithUserRole extends Request {
+  user?: JWTPayload;
+}
 
-  if (!token) {
-    throw new CustomError(403, "Missing token please provide a token");
-  }
-
-  jwt.verify(
-    token,
-    "1234567890",
-    (err, decoded) => {
-      if (err) {
-        throw new CustomError(400, "Invalid token");
+const authenticateJWT =
+  () =>
+  async (req: RequestWithUserRole, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers?.authorization?.split(" ")[1];
+      // console.log(req.headers?.authorization);
+      if (!token) {
+        return res.status(403).json({ message: "Token not  found" });
       }
-
-      // Assuming JWTPayload is your payload type
-      req.user = decoded as JWTPayload;
-
-      next();
+      const decodedData = <JWTPayload>jwt.verify(token, '123456789');
+      req.user = decodedData;
+      // console.log(decodedData);
+      return next();
+    } catch (e) {
+      return res.status(500).json({ e });
     }
-  );
-};
+  };
 
 export default authenticateJWT;
